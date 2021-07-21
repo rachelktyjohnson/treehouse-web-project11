e55d2f638d43676863406c988e7225f5304a236e
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-07-21 20:59:15 +1000

    Change up default topics

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -4,7 +4,7 @@ function MainNav(){
     return(
         <nav className="main-nav">
             <ul>
-                <li><NavLink to="/search/teddybear">Teddybears</NavLink></li>
+                <li><NavLink to="/search/fish">Fish</NavLink></li>
                 <li><NavLink to="/search/elephants">Elephants</NavLink></li>
                 <li><NavLink to="/search/hippos">Hippos</NavLink></li>
             </ul>
